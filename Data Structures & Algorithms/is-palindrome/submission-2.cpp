8cd815a53ee1e26c77ced854a9cbe52926e379b7
Ayush Bhujade <156687702+bhujade-ayush@users.noreply.github.com>
2026-08-16 15:15:29 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAlphanumeric(char c) {
+        if((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z' || (c >= 'a' && c <= 'z'))) return true;
+        return false;
+    }
+
+    bool isPalindrome(string s) {
+        int i = 0, j = s.length()-1;
+
+        for(auto& c: s){
+            c = tolower(c);
+        }
+
+        while(i < j){
+            while(i < j && !isAlphanumeric(s[i])) i++;
+            while(i < j && !isAlphanumeric(s[j])) j--;
+            if(i < j && s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+};
